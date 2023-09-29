49ee985c4f2e01c48286e73ec0301b215674f1a6
blondine-sor <bkeumani@gmail.com>
2023-09-29 10:53:22 -0400

    Creation Formulaire Resultat

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+?>
+ <form method="post" action="formResult.php">
+    <legend><h1>Formulaire</h1></legend>
+    <fieldset>
+        
+        <label for="firstName">FirstName</label>
+        <input id="firstName" name="nom" value="">
+</br>
+        <label for="surName">Surname</label>
+        <input id="surName" name="prenom" value="">
+    </fieldset>
+ </form>
+
+<?php
+
+
+
+
+
+
+
+
+
+ ?>
